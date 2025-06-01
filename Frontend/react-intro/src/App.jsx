31b288bd10bafc1b0aafdf3bbc8bc1534dada0ca
Kabita Adk <kabitaadk55@gmail.com>
2025-06-01 07:56:15 +0545

    usestate and Array map function

--- Frontend/react-intro/src/App.jsx
+++ Frontend/react-intro/src/App.jsx
@@ -1,31 +1,28 @@
+
 import { useState } from "react";
 
-function Quotes() {
-  const [quotes, setQuotes] = useState([
-    "Stay hungry",
-    "Be Kind",
-    "Keep coding"
-  ]);
 
-  return (
+function QuotesApp(){
+  const [quotes, setQuotes] = useState([]);
+  return(
     <div>
       <h1>Quotes</h1>
-      <ul>
-        {quotes.map((quote, index) => (
-          <li key={index}>{quote}</li>
-        ))}
-      </ul>
+      <form onSubmit={(event)=>{
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const quote ={
+          id: quotes.length + 1,
+          quote: formData.get('quote'),
+        };
+        setQuotes([... quotes, quote]);
+      }}>
+        <input type="text" name="quote" placeholder="Add a quote"/>
+        <button type="submit"> Add quote </button>
+      </form>
+      {quotes.map((quote)=>quote.quote)}
     </div>
   );
 }
 
-export default Quotes;
+export default QuotesApp;
 
-// template literals
-// spread operator
-//  default parameter
-// role operator
-// destructuring
-// arrow function
-// callback function
-// ternary operator
